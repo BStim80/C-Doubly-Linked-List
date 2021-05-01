00f66bf492e62f737acaafca95944695ac679bb4
Robert Stimmel <B.Stimmel80@gmail.com>
2021-04-30 22:05:25 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C__Doubly_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
